4e58ba25dc7f888c7ecbac7b1220c6ee754c0001
Tadashi <shinobi.design416@gmail.com>
2024-07-09 00:16:57 -0700

    Fixed styles page and updated style variables

--- src/pages/styles/index.tsx
+++ src/pages/styles/index.tsx
@@ -1,133 +1,52 @@
 import "./style.css";
 
+interface Color {
+  name: string;
+  shades: number[];
+}
+
+const colors: Color[] = [
+  {
+    name: "primary",
+    shades: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+  },
+  {
+    name: "success",
+    shades: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+  },
+  {
+    name: "danger",
+    shades: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+  },
+  {
+    name: "warning",
+    shades: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+  },
+];
+
 export default function StylesPage() {
   return (
     <div>
       Styles page
-      <div className="card-box">
-        <div
-          className="card"
-          style={{
-            backgroundColor: "var(--color-primary-100)",
-          }}
-        />
-        <div
-          className="card"
-          style={{
-            backgroundColor: "var(--color-primary-200)",
-          }}
-        />
-        <div
-          className="card"
-          style={{
-            backgroundColor: "var(--color-primary-300)",
-          }}
-        />
-        <div
-          className="card"
-          style={{
-            backgroundColor: "var(--color-primary-400)",
-          }}
-        />
-        <div
-          className="card"
-          style={{
-            backgroundColor: "var(--color-primary-500)",
-          }}
-        />
-        <div
-          className="card"
-          style={{
-            backgroundColor: "var(--color-primary-600)",
-          }}
-        />
-        <div
-          className="card"
-          style={{
-            backgroundColor: "var(--color-primary-700)",
-          }}
-        />
-        <div
-          className="card"
-          style={{
-            backgroundColor: "var(--color-primary-800)",
-          }}
-        />
-        <div
-          className="card"
-          style={{
-            backgroundColor: "var(--color-primary-900)",
-          }}
-        />
-        <div
-          className="card"
-          style={{
-            backgroundColor: "var(--color-primary)",
-          }}
-        />
-      </div>
-      <div className="card-box">
-        <div
-          className="card"
-          style={{
-            backgroundColor: "var(--color-success-100)",
-          }}
-        />
-        <div
-          className="card"
-          style={{
-            backgroundColor: "var(--color-success-200)",
-          }}
-        />
-        <div
-          className="card"
-          style={{
-            backgroundColor: "var(--color-success-300)",
-          }}
-        />
-        <div
-          className="card"
-          style={{
-            backgroundColor: "var(--color-success-400)",
-          }}
-        />
-        <div
-          className="card"
-          style={{
-            backgroundColor: "var(--color-success-500)",
-          }}
-        />
-        <div
-          className="card"
-          style={{
-            backgroundColor: "var(--color-success-600)",
-          }}
-        />
-        <div
-          className="card"
-          style={{
-            backgroundColor: "var(--color-success-700)",
-          }}
-        />
-        <div
-          className="card"
-          style={{
-            backgroundColor: "var(--color-success-800)",
-          }}
-        />
-        <div
-          className="card"
-          style={{
-            backgroundColor: "var(--color-success-900)",
-          }}
-        />
-        <div
-          className="card"
-          style={{
-            backgroundColor: "var(--color-success)",
-          }}
-        />
-      </div>
+      {colors.map((color) => (
+        <div key={color.name} className="card-box">
+          {color.shades.map((shade) => (
+            <div
+              key={`${color.name}-${shade}`}
+              className="card"
+              style={{
+                backgroundColor: `var(--color-${color.name}-${shade})`,
+              }}
+            />
+          ))}
+          <div
+            className="card"
+            style={{
+              backgroundColor: `var(--color-${color.name})`,
+            }}
+          />
+        </div>
+      ))}
     </div>
   );
 }
